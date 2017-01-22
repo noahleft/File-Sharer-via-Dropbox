62bdefb90cdbed71ee797856bf613250659d35bf
ShihFengLin <noahleft@gmail.com>
2017-01-23 02:01:28 +0800

    Initial Commit

---
+++ FileSharer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  FileSharer
+//
+//  Created by 林世豐 on 23/01/2017.
+//  Copyright © 2017 林世豐. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ FileSharer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FileSharer
+//
+//  Created by 林世豐 on 23/01/2017.
+//  Copyright © 2017 林世豐. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
